9a9895235020e5d3812fa9aafd5c8f03e88e3dbd
Franky So <frankyso.mail@gmail.com>
2019-11-28 04:18:55 +0000

    Apply fixes from StyleCI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,13 @@
 /**
  * Copyright (c) 2019.
  */
-
 spl_autoload_register(function ($class) {
-    $file = __DIR__ . '/../src/' . strtr($class, '\\', '/') . '.php';
+    $file = __DIR__.'/../src/'.strtr($class, '\\', '/').'.php';
     if (file_exists($file)) {
         require $file;
+
         return true;
     }
+
     return true;
 });
